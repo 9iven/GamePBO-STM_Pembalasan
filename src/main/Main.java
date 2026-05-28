066b38791ce48ff8dc7c8b5ef9910ee556f9c375
Given Gerald Chandra <givengerald947@gmail.com>
2026-05-28 23:55:21 +0700

    Fitur FullScreen

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,27 +3,22 @@ package main;
 import gameengine.Game;
 import javax.swing.JFrame;
 
-// Entry point program
 public class Main {
 
     public static void main(String[] args) {
 
         JFrame window = new JFrame("STM Pembalasan - Proyek Akhir PBO");
-
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
+
+        // Mengizinkan window di-resize agar centering dapat bekerja dinamis
+        window.setResizable(true);
 
         Game gamePanel = new Game();
-
         window.add(gamePanel);
-
         window.pack();
-
         window.setLocationRelativeTo(null);
-
         window.setVisible(true);
 
-        // Memulai game loop
         gamePanel.startGameLoop();
     }
 }
